c73ed803a8c795155807aa780fec15667024fd75
CiroboyBR <ciroccuva@hotmail.com>
2021-03-06 21:26:32 -0300

    Usando metodos count, find e any.

--- src/main/kotlin/one/digitalinovation/collections/TesteOperacaoCollections.kt
+++ src/main/kotlin/one/digitalinovation/collections/TesteOperacaoCollections.kt
@@ -12,6 +12,18 @@ fun main() {
 
     val salarioAlto = salarios.filter { it > 2000.0 }
     println("------------------")
-    salarioAlto.forEach{println(it)}
+    salarioAlto.forEach { println(it) }
+
+    println("------------------")
+    println(salarios.count { it in 2000.0..5000.0 })
+    println("Igual a 3000 ------------------")
+    println(salarios.find { it == 3000.0 })
+    println("Diferente de 3000 ------------------")
+    println(salarios.find { it != 3000.0 })
+    println("Tem algum elemento igual a 3k? ------------------------")
+    println(salarios.any { it == 3000.0 })
+    println("Tem algum elemento igual a 9k? ------------------------")
+    println(salarios.any { it == 9000.0 })
+
 
 }
