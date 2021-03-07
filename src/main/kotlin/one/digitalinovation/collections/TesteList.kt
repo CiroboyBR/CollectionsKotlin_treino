321824119a4253e283f95c823d3ec966c3b8a351
CiroboyBR <ciroccuva@hotmail.com>
2021-03-07 03:23:50 -0300

    Usando Union e subtract em conjuntos

--- src/main/kotlin/one/digitalinovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinovation/collections/TesteList.kt
@@ -15,15 +15,3 @@ fun main() {
     funcionarios.groupBy { it.tipoContratacao }.forEach { println(it) }
 }
 
-data class Funcionario(
-    val nome: String,
-    val salario: Double,
-    val tipoContratacao: String
-) {
-    override fun toString(): String =
-        """
-            Nome: $nome
-            Salário: $salario
-            Contrato: $tipoContratacao
-        """.trimIndent()
-    }
---
+++ src/main/kotlin/one/digitalinovation/collections/testSet.kt
@@ -0,0 +1,17 @@
+package one.digitalinovation.collections
+
+fun main() {
+    val joao = Funcionario("joao", 3000.0, "CLT")
+    val pedro = Funcionario("pedro", 4000.0, "PJ")
+    val maria = Funcionario("Maria", 2000.0, "CLT")
+
+    val funcionariosMasculinos = setOf(joao, pedro)
+    val funcionariosFemininos = setOf(maria)
+
+    val funcionariosTodos = funcionariosMasculinos.union(funcionariosFemininos)
+    funcionariosTodos.forEach { println(it) }
+
+    println("Subtract --------------------------")
+    val funcionariosSubtraidos = funcionariosTodos.subtract(funcionariosFemininos)
+    funcionariosSubtraidos.forEach { println(it) }
+}
